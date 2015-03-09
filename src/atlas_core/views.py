f25ce77af344ecbde4eedab760da84f7be4c55f6
Mali Akmanalp <mali@akmanalp.com>
2015-03-09 10:31:31 -0400

    Remove flask-restful, use raw flask and marshmallow.

--- src/atlas_core/views.py
+++ src/atlas_core/views.py
@@ -1,29 +1,27 @@
-from flask.ext import restful
-from flask.ext.restful import fields, marshal_with
 from flask import Blueprint, jsonify
+import marshmallow as m
 from atlas_core.models import Cat
 
 main_app = Blueprint("main_app", __name__)
 
-cat_fields = {
-    'id': fields.String,
-    'born_at': fields.Integer,
-    'name': fields.String,
-}
+
+class CatSchema(m.Schema):
+    id = m.fields.Str()
+    born_at = m.fields.Int()
+    name = m.fields.Str()
+
+cat_schema = CatSchema()
 
 
-class CatAPI(restful.Resource):
+@main_app.route("/cats/<int:cat_id>")
+def cat(cat_id):
+    """Get a :py:class:`~atlas_core.models.Cat` with the given cat ID.
 
-    @marshal_with(cat_fields)
-    def get(self, cat_id):
-        """Get a :py:class:`~atlas_core.models.Cat` with the given cat ID.
+    :param id: unique ID of the cat
+    :type id: int
+    :code 404: cat doesn't exist
 
-        :param id: unique ID of the cat
-        :type id: int
-        :code 404: cat doesn't exist
+    """
+    q = Cat.query.get_or_404(cat_id)
+    return jsonify(cat_schema.dump(q).data)
 
-        """
-
-        q = Cat.query.get_or_404(cat_id)
-
-        return q
