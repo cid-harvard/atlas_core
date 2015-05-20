5dac21ed15bf9deaad36a0f4049451909baf7f1a
Mali Akmanalp <mali@akmanalp.com>
2015-05-20 14:02:48 -0400

    Make BaseTestCase more configurable by overriding

--- atlas_core/tests.py
+++ atlas_core/tests.py
@@ -10,8 +10,14 @@ class BaseTestCase(TestCase):
     asserts. Uses app factory and creates / tears down db.
     """
 
+    SQLALCHEMY_DATABASE_URI = "sqlite://"
+    ADDITIONAL_CONFIG = {
+        "SQLALCHEMY_DATABASE_URI": SQLALCHEMY_DATABASE_URI,
+        "TESTING": True
+    }
+
     def create_app(self):
-        return create_app(additional_config={"TESTING": True})
+        return create_app(additional_config=self.ADDITIONAL_CONFIG)
 
     def setUp(self):
         db.create_all()
