833b1dddd0b96b695efb2c548dcd63abdebbbcf4
Brendan Leonard <brendan_leonard@hks.harvard.edu>
2018-06-28 15:40:33 -0400

    Set db.metadata naming convention to automatically name constraints so we can drop them during loading

--- atlas_core/core.py
+++ atlas_core/core.py
@@ -4,3 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 #: Flask-SQLAlchemy db object
 db = SQLAlchemy()
+
+# Need these to be generated in order to drop them when loading
+db.metadata.naming_convention = {
+    'ix': '%(table_name)s_%(column_0_name)s_idx',
+    "fk": "%(table_name)s_%(column_0_name)s_fkey",
+    "pk": "%(table_name)s_pkey"
+}
