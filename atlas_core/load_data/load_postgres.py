99a6cee1763c099c12e9ed47260c8c8aa4338adc
Brendan Leonard <brendan_leonard@hks.harvard.edu>
2018-07-06 12:35:56 -0400

    WIP, mostly tweaks with handling partner data loading

--- atlas_core/load_data/load_postgres.py
+++ atlas_core/load_data/load_postgres.py
@@ -1,13 +1,23 @@
-from .utilities import create_file_object, cast_pandas, logger,\
-                       classification_to_pandas, hdf_metadata, df_generator
+from .utilities import (create_file_object, df_generator, logger, hdf_metadata,
+                        classification_to_pandas, cast_pandas,
+                        add_level_metadata)
 from atlas_core import db
 
 import pandas as pd
-import numpy as np
 
 from sqlalchemy.schema import AddConstraint, DropConstraint
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.engine import reflection
+
+
+def commit(session):
+    session.commit()
+    session.execute("SET maintenance_work_mem TO 1000000;")
+
+
+def rollback(session):
+    session.rollback()
+    session.execute("SET maintenance_work_mem TO 1000000;")
 
 
 def copy_to_database(session, table, columns, file_object):
@@ -30,6 +40,20 @@ def update_level_fields(db, hdf_table, sql_table, levels):
     return update_obj
 
 
+def chunk_copy_df(session, df, sql_table, chunksize):
+    logger.info("Creating generator for chunking dataframe")
+    for chunk in df_generator(df, chunksize, logger=logger):
+
+        logger.info("Creating CSV in memory")
+        fo = create_file_object(chunk)
+
+        logger.info("Copying chunk to database")
+        copy_to_database(session, sql_table, df.columns, fo)
+        del fo
+
+        logger.info("Chunk copied successfully")
+
+
 def drop_foreign_keys(session):
     '''
     Drop all foreign keys in a database.
@@ -37,6 +61,7 @@ def drop_foreign_keys(session):
     Parameters
     ----------
     session: SQLAlchemy db session
+    logger: logging Logger object
 
     Returns
     -------
@@ -49,14 +74,21 @@ def drop_foreign_keys(session):
     for sql_table in insp.get_table_names():
         fks = db.metadata.tables[sql_table].foreign_key_constraints
         for fk in fks:
-            session.execute(DropConstraint(fk))
+            try:
+                session.execute(DropConstraint(fk))
+                commit(session)
+                logger.info("Dropped foreign key %s", fk.name)
+            except SQLAlchemyError:
+                logger.warn("Foreign key %s not found", fk.name)
+                rollback(session)
+
         db_foreign_keys += fks
 
     return db_foreign_keys
 
 
 def copy_to_postgres(sql_table, session, sql_to_hdf, file_name, levels,
-                     chunksize, commit_every):
+                     chunksize, hdf_chunksize, commit_every):
     '''Copy all HDF tables relating to a single SQL table to database'''
 
     table_obj = db.metadata.tables[sql_table]
@@ -76,13 +108,13 @@ def copy_to_postgres(sql_table, session, sql_to_hdf, file_name, levels,
     rows = 0
 
     if hdf_tables is None:
-        logger.info("No HDF table found for SQL table %s", sql_table)
+        logger.warn("No HDF table found for SQL table %s", sql_table)
         return rows
 
     for hdf_table in hdf_tables:
         logger.info("*** %s ***", hdf_table)
 
-        # Handle classifications differently
+        # Handle classifications formatting differently and read all at once
         if hdf_table.startswith("/classifications/"):
             logger.info("Reading HDF table")
             df = pd.read_hdf(file_name, key=hdf_table)
@@ -104,40 +136,37 @@ def copy_to_postgres(sql_table, session, sql_to_hdf, file_name, levels,
 
         # Read partner HDF tables as iterators to conserve memory
         elif 'partner' in hdf_table:
+
             hdf_levels = levels.get(hdf_table)
 
-            logger.info("Reading HDF table %s", hdf_table)
-            iterator = pd.read_hdf(file_name, key=hdf_table,
-                                   chunksize=chunksize, iterator=True)
+            with pd.HDFStore(file_name) as store:
+                nrows = store.get_storer(hdf_table).nrows
 
-            n_chunks = (iterator.nrows // chunksize) + 1
+            n_chunks = (nrows // hdf_chunksize) + 1
+            rows += nrows
 
-            for i, df in enumerate(iterator):
-                logger.info("*** Chunk %(i)s of %(n)s ***",
-                            {'table': hdf_table, 'i': i + 1, 'n': n_chunks})
-                rows += len(df)
+            for i in range(n_chunks):
+                logger.info("*** HDF chunk %(i)s of %(n)s ***",
+                            {'i': i + 1, 'n': n_chunks})
 
-                # Add columns from level metadata to df
-                if hdf_levels:
-                    logger.info("Adding level metadata values")
-                    for entity, level_value in hdf_levels.items():
-                        df[entity + "_level"] = level_value
+                start = i * hdf_chunksize
+                stop = min(start + hdf_chunksize, nrows)
+
+                logger.info("Reading HDF table")
+                df = pd.read_hdf(file_name, key=hdf_table,
+                                 start=start, stop=stop)
 
                 # Convert fields that should be int to object fields
                 df = cast_pandas(df, table_obj)
-                columns = df.columns
 
-                logger.info("Creating CSV in memory")
-                fo = create_file_object(df)
-                del df
+                # Add columns from level metadata to df
+                hdf_levels = levels.get(hdf_table)
+                df = add_level_metadata(df, hdf_levels)
 
-                logger.info("Copying table to database")
-                copy_to_database(session, sql_table, columns, fo)
-                del fo
+                chunk_copy_df(session, df, sql_table, chunksize)
 
+        # Read entire HDF file, but use generator for iterating chunks to CSV
         else:
-            hdf_levels = levels.get(hdf_table)
-
             logger.info("Reading HDF table")
             df = pd.read_hdf(file_name, key=hdf_table)
             rows += len(df)
@@ -146,22 +175,10 @@ def copy_to_postgres(sql_table, session, sql_to_hdf, file_name, levels,
             df = cast_pandas(df, table_obj)
 
             # Add columns from level metadata to df
-            if hdf_levels:
-                logger.info("Adding level metadata values")
-                for entity, level_value in hdf_levels.items():
-                    df[entity + "_level"] = level_value
+            hdf_levels = levels.get(hdf_table)
+            df = add_level_metadata(df, hdf_levels)
 
-            # Split dataframe before creating StringIO objects in memory
-            columns = df.columns
-
-            for chunk in df_generator(df, chunksize, logger=logger):
-
-                logger.info("Creating CSV in memory")
-                fo = create_file_object(chunk)
-
-                logger.info("Copying chunk to database")
-                copy_to_database(session, sql_table, columns, fo)
-                del fo
+            chunk_copy_df(session, df, sql_table, chunksize)
 
     logger.info("All chunks copied (%s rows)", rows)
 
@@ -171,14 +188,13 @@ def copy_to_postgres(sql_table, session, sql_to_hdf, file_name, levels,
 
     if commit_every:
         logger.info("Committing transaction.")
-        session.commit()
-        session.execute("SET maintenance_work_mem TO 1000000;")
+        commit(session)
 
     return rows
 
 
-def hdf_to_postgres(file_name="./data.h5", chunksize=10**6, keys=None,
-                    commit_every=True):
+def hdf_to_postgres(file_name="./data.h5", keys=None, chunksize=10**6,
+                    hdf_chunksize=10**7, commit_every=True):
     '''
     Copy a HDF file to a postgres database
 
@@ -186,10 +202,12 @@ def hdf_to_postgres(file_name="./data.h5", chunksize=10**6, keys=None,
     ----------
     file_name: str
         path to a HDFfile to copy to database
-    chunksize: int
-        max number of rows to read/copy in any transaction
     keys: iterable
         set of keys in HDF to limit load to
+    chunksize: int
+        max number of rows to read/copy in any transaction
+    hdf_chunksize: int
+        when reading HDF file in chunks, max row count
     commit_every: boolean
         if true, commit after every major transaction (e.g., table COPY)
     '''
@@ -202,20 +220,20 @@ def hdf_to_postgres(file_name="./data.h5", chunksize=10**6, keys=None,
 
     # Drop all foreign keys first to allow for dropping PKs after
     logger.info("Dropping foreign keys for all tables")
-    db_foreign_keys = drop_foreign_keys(session)
+    db_foreign_keys = drop_foreign_keys(session, logger)
 
     rows = 0
 
     for sql_table in sql_to_hdf.keys():
         rows += copy_to_postgres(sql_table, session, sql_to_hdf, file_name,
-                                 levels, chunksize, commit_every)
+                                 levels, chunksize, hdf_chunksize,
+                                 commit_every)
 
     # Add foreign keys back in after all data loaded to not worry about order
     logger.info("Recreating foreign keys on all tables")
     for fk in db_foreign_keys:
-        session.execute("SET maintenance_work_mem TO 1000000;")
         session.execute(AddConstraint(fk))
-        session.commit()
+        commit(session)
 
     # Set this back to default value
     logger.info("Job complete. %s rows copied to db.", rows)
