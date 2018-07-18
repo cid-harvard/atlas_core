ef2a43291c2a5db071b107223d62d6713180dddf
Brendan Leonard <brendan_leonard@hks.harvard.edu>
2018-07-18 11:42:58 -0400

    Update script to use refactored metadata

--- atlas_core/hdf_to_postgres.py
+++ atlas_core/hdf_to_postgres.py
@@ -1,23 +1,29 @@
+import logging
 from atlas_core import db
-from typing import List, Callable
-from pandas import DataFrame
 from multiprocessing import Pool
 from sqlalchemy.exc import SQLAlchemyError
 from pandas_to_postgres import (
     HDFTableCopy,
     SmallHDFTableCopy,
     BigHDFTableCopy,
-    HDFMetadata,
-    logger,
     cast_pandas,
+    hdf_metadata,
+    copy_worker,
 )
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s.%(msecs)03d - %(name)s - %(levelname)s %(message)s",
+    datefmt="%Y-%m-%d,%H:%M:%S",
+)
+
+logger = logging.getLogger("hdf_to_postgres")
+
 
 e = db.engine
 
 
-def add_level_metadata(
-    df: DataFrame, copy_obj: HDFTableCopy, hdf_table: str, **kwargs
-) -> DataFrame:
+def add_level_metadata(df, copy_obj, hdf_table, **kwargs):
     """
     Updates dataframe fields for constant "_level" fields
 
@@ -75,40 +81,48 @@ def classification_to_pandas(
     return new_df
 
 
-def create_table_objects(hdf_meta, csv_chunksize=10 ** 6):
+def create_table_objects(
+    file_name, sql_to_hdf, csv_chunksize=10 ** 6, hdf_chunksize=10 ** 7, hdf_meta=None
+):
     classifications = []
     partners = []
     other = []
 
-    for sql_table, hdf_tables in hdf_meta.sql_to_hdf.items():
+    for sql_table, hdf_tables in sql_to_hdf.items():
         if any("classifications/" in table for table in hdf_tables):
             classifications.append(
                 SmallHDFTableCopy(
+                    file_name,
                     hdf_tables,
-                    hdf_meta,
                     defer_sql_objs=True,
                     sql_table=sql_table,
                     csv_chunksize=csv_chunksize,
+                    hdf_chunksize=hdf_chunksize,
+                    hdf_metadata=hdf_meta,
                 )
             )
         elif any("partner" in table for table in hdf_tables):
             partners.append(
                 BigHDFTableCopy(
+                    file_name,
                     hdf_tables,
-                    hdf_meta,
                     defer_sql_objs=True,
                     sql_table=sql_table,
                     csv_chunksize=csv_chunksize,
+                    hdf_chunksize=hdf_chunksize,
+                    hdf_metadata=hdf_meta,
                 )
             )
         else:
             other.append(
                 HDFTableCopy(
+                    file_name,
                     hdf_tables,
-                    hdf_meta,
                     defer_sql_objs=True,
                     sql_table=sql_table,
                     csv_chunksize=csv_chunksize,
+                    hdf_chunksize=hdf_chunksize,
+                    hdf_metadata=hdf_meta,
                 )
             )
 
@@ -116,65 +130,67 @@ def create_table_objects(hdf_meta, csv_chunksize=10 ** 6):
     return classifications, partners + other
 
 
-def copy_worker(
-    copy_obj: HDFTableCopy,
-    multiprocess: bool = True,
-    data_formatters: List[Callable] = [add_level_metadata, cast_pandas],
-):
-    e.dispose()
-    with e.connect() as conn:
-        conn.execution_options(autocommit=True)
-        conn.execute("SET maintenance_work_mem TO 1000000;")
-
-        if multiprocess:
-            table_obj = db.metadata.tables[copy_obj.sql_table]
-            copy_obj.instantiate_sql_objs(conn, table_obj)
-
-        copy_obj.copy(data_formatters=data_formatters)
-
-
 def hdf_to_postgres(
-    file_name: str = "./data.h5",
-    keys: List[str] = [],
+    file_name="./data.h5",
+    keys=[],
+    processes=4,
+    engine_args=[db.engine.url],
+    engine_kwargs={},
+    maintenance_work_mem="1GB",
     hdf_chunksize: int = 10 ** 7,
     csv_chunksize: int = 10 ** 6,
 ):
-    hdf = HDFMetadata(
-        file_name,
-        keys,
-        hdf_chunksize,
-        metadata_attr="atlas_metadata",
-        metadata_keys=["levels"],
+
+    sql_to_hdf, metadata_vars = hdf_metadata(
+        file_name, keys=keys, metadata_attr="atlas_metadata", metadata_keys=["levels"]
     )
-    classifications, tables = create_table_objects(hdf, csv_chunksize=csv_chunksize)
 
-    with e.connect() as conn:
-        conn.execution_options(autocommit=True)
-        conn.execute("SET maintenance_work_mem TO 1000000;")
+    classifications, tables = create_table_objects(
+        file_name,
+        sql_to_hdf,
+        csv_chunksize=csv_chunksize,
+        hdf_chunksize=hdf_chunksize,
+        hdf_meta=metadata_vars,
+    )
 
-        for ct in classifications:
-            table_obj = db.metadata.tables[ct.sql_table]
-            ct.instantiate_sql_objs(conn, table_obj)
-            # Order of data_formatters list matters here
-            copy_worker(
-                ct,
-                multiprocess=False,
-                data_formatters=[classification_to_pandas, cast_pandas],
-            )
+    # Copy classifications first, not multiprocessed
+    for class_table in classifications:
+        # Order of data_formatters list matters here
+        copy_worker(
+            class_table,
+            engine_args,
+            engine_kwargs,
+            maintenance_work_mem=maintenance_work_mem,
+            data_formatters=[classification_to_pandas, cast_pandas],
+        )
 
+    # Use multiprocessing for the larger tables once classifications are complete
+    args = zip(
+        tables,
+        [engine_args] * len(tables),
+        [engine_kwargs] * len(tables),
+        [maintenance_work_mem] * len(tables),
+    )
     try:
-        p = Pool(4)
-        p.imap_unordered(copy_worker, tables, chunksize=1)
-    except Exception as ex:
-        logger.exception(ex)
+        p = Pool(processes)
+        result = p.starmap_async(copy_worker, args, chunksize=1)
     finally:
         del tables
-        del hdf
         p.close()
         p.join()
+    if not result.successful():
+        # If there's an exception, throw it, but we don't care about the
+        # results
+        result.get()
 
 
 if __name__ == "__main__":
     hdf_to_postgres(
-        file_name="./data.h5", keys=None, hdf_chunksize=10 ** 7, csv_chunksize=10 ** 6
+        file_name="./data.h5",
+        keys=None,
+        processes=4,
+        engine_args=[db.engine.url],
+        maintenance_work_mem="1GB",
+        hdf_chunksize=10 ** 7,
+        csv_chunksize=10 ** 6,
     )
